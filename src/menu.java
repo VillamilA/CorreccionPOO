a54f8a2938818907bdf95d4b8d37238995c13dab
VillamilA <angel.villamil@epn.edu.ec>
2024-07-17 21:44:35 -0500

    Menu despues del login

---
+++ src/menu.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class menu extends JFrame{
+    private JButton registrarPacienteButton;
+    private JButton buscarPacienteButton;
+    private JButton eliminarPacienteButton;
+    private JButton salirButton;
+    private JButton actualizarPacienteButton;
+    private JPanel menuu;
+
+    public menu() {
+        setTitle("Menu");
+        setSize(650,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setContentPane(menuu);
+        registrarPacienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new registrarPaciente().setVisible(true);
+                dispose();
+            }
+        });
+        actualizarPacienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new actualizarpaciente().setVisible(true);
+            }
+        });
+        buscarPacienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new buscarpaciente().setVisible(true);
+            }
+        });
+        eliminarPacienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new eliminarpaciente().setVisible(true);
+            }
+        });
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
