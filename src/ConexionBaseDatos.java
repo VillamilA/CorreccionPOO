8fcc30862db352d6b06282e453bf906d7edf8cf6
VillamilA <angel.villamil@epn.edu.ec>
2024-07-17 21:43:46 -0500

    Creacion GIT y conexion

---
+++ src/ConexionBaseDatos.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBaseDatos {
+    private static final String URL = "jdbc:mysql://localhost:3306/sistema_hospitalario";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
