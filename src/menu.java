b54a359b2baaa1a4962bb45d2fad0435dfd1376b
VillamilA <angel.villamil@epn.edu.ec>
2024-07-17 21:59:59 -0500

    boton salir, slga del programa

--- src/menu.java
+++ src/menu.java
@@ -43,7 +43,7 @@ public class menu extends JFrame{
         salirButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                System.exit(0);
             }
         });
     }
